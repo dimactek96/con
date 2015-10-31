986fe50081c16769f5121294c6ef7cbe1333a62c
MrOrlovich <dim5200@yandex.ru>
2015-10-31 23:07:33 +0300

    пункт6

---
+++ convert/main.cpp
@@ -0,0 +1,40 @@
+#include <conio.h>
+#include <iostream>
+#include <vector>
+#include <exception>
+#include <stdexcept>
+#include <limits>
+#include <G:\C++\lab3\punct6\convert\realiz.h>
+using std::cout;
+using std::endl;
+using std::cin;
+using namespace std;
+int main()
+{
+    try
+    {
+        setlocale(LC_ALL,"Russian");
+        double znach_t,resultat;
+        char frm,too;
+        cout<<"¬ведите температуру :\n";
+        cin>>znach_t;
+        cout<<"¬ведите шкалу введенной температуры :\n";
+        cin>>frm;
+        cout<<"¬ведите шкалу в которую перевести тепературу :\n";
+        cin>>too;
+        resultat=convert(znach_t,frm,too);
+        cout<<"«начение температуры="<<resultat;
+    }
+    catch(invalid_argument& e)
+    {
+        cerr <<e.what()<<"\n";
+    }
+    catch(logic_error& le)
+    {
+        cerr <<le.what()<<"\n";
+    }
+    catch(exception& e)
+    {
+        cerr << e.what()<<"\n";
+    }
+}
